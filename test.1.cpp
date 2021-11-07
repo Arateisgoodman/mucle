b1b4ba98896c0f11774a615427b823dfe2c1ac8f
Arateisgoodman <89933624+Arateisgoodman@users.noreply.github.com>
2021-11-07 22:14:59 +0800

    Add files via upload

---
+++ test.1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	printf("~~~~~~~~~~~~~~~~~~~~\n");
+	printf("        董世豪\n");
+	printf("Tel:16663799820\n");
+	printf("系别:机械系\n");
+	printf("专业：机械大类\n");
+	printf("学号：B20121322\n");
+	printf("~~~~~~~~~~~~~~~~~~~\n");
+	return 0;
+	
+}
